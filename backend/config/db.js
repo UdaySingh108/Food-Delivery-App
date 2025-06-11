547c56adeed31fe701581237ea27c6253733b9ed
UdaySingh108 <singh.uday2610@gmail.com>
2025-06-11 11:41:57 +0530

    update  security

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
   try {
     // Connect to MongoDB without deprecated options
-    await mongoose.connect('mongodb+srv://singhuday2610:Uday2610@cluster0.pax4s.mongodb.net/FoodDeliveryApp');
+    await mongoose.connect('mongodb+srv://singhuday2610:db_pass@cluster0.pax4s.mongodb.net/FoodDeliveryApp');
     console.log("Uday DB connected successfully!");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
