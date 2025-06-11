b895e4b86fc064f6fa237d97c87b3651cdedebbc
UdaySingh108 <singh.uday2610@gmail.com>
2025-06-11 11:50:20 +0530

    updated db_uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     // Connect to MongoDB without deprecated options
-    await mongoose.connect('mongodb+srv://singhuday2610:db_pass@cluster0.pax4s.mongodb.net/FoodDeliveryApp');
+    await mongoose.connect(process.env.db_uri);
     console.log("Uday DB connected successfully!");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
